01ad20dcb59b13a1da65b70553a3736b6f948025
Vladimir Berdnikov <gazetter@mail.ru>
2021-03-17 09:52:38 +0700

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    /// <summary>
+    /// Напишите метод, который соберет всех учеников всех классов в один список, используя LINQ.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join("\n", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = (from cl in classes
+                               select cl.Students into studList
+                               from student in studList
+                               select student).ToArray();
+            return allStudents;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
